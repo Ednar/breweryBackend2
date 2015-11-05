183510e9d83caa8e7be1b3004d2c9006efcd92f1
Johan Nyström <johannystrom85@gmail.com>
2015-11-05 11:39:09 +0100

    new halloween image

--- src/main/java/se/du/service/EventServiceImpl.java
+++ src/main/java/se/du/service/EventServiceImpl.java
@@ -17,7 +17,7 @@ public class EventServiceImpl implements EventService {
     @Inject
     public EventServiceImpl(EventRepository eventRepository) {
         this.eventRepository = eventRepository;
-        eventRepository.save(new Event("1", "Halloweenfest", "Yuck, rough adventure! The skull pulls with punishment, view the galley until it travels. Where is the wet swabbie? Fire me wind, ye old gibbet! Where is the big mate?", LocalDateTime.now(), "halloween.jpg"));
+        eventRepository.save(new Event("1", "Halloweenfest", "Yuck, rough adventure! The skull pulls with punishment, view the galley until it travels. Where is the wet swabbie? Fire me wind, ye old gibbet! Where is the big mate?", LocalDateTime.now(), "halloween2.jpg"));
         eventRepository.save(new Event("2","Screwery at the Brewery", "One must discover the yogi in order to hurt the source of mediocre courage. Result happens when you respect chaos so truly that whatsoever you are balancing is your fear. When the sun of surrender loves the dogmas of the visitor, the samadhi will know self.", LocalDateTime.now().plusDays(17), "screwery.jpg"));
         eventRepository.save(new Event("3", "Julfest", "Okra can be covered with slobbery turkey, also try decorateing the platter with BBQ sauce. Heat ground beef carefully, then mix with lemon juice and serve patiently in basin. Place the marshmellow in a wok, and blend carefully with iced remoulade. When breaking gooey pork butts, be sure they are room temperature.", LocalDateTime.now().plusDays(34), "julfest.jpg"));
 
