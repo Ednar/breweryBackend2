046eacb72caed7ffc01cc68090a02ce2b19d6bcd
Johan Nyström <johannystrom85@gmail.com>
2015-11-06 12:39:33 +0100

    fixed getEventWithID

--- src/main/java/se/du/controller/EventController.java
+++ src/main/java/se/du/controller/EventController.java
@@ -2,10 +2,7 @@ package se.du.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se.du.domain.Event;
 import se.du.service.EventService;
 
@@ -23,7 +20,7 @@ public class EventController {
     }
 
     @RequestMapping(value = "events/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Event getEventWithId(@RequestParam("id") String id) {
+    public Event getEventWithId(@PathVariable("id") String id) {
         return eventService.getEventById(id);
     }
 
